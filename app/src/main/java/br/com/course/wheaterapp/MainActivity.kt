3528a2be814294ff53f83a533ad9cbe90d9b6439
Arthur Braga <ahbragadev@gmail.com>
2023-10-10 12:06:15 -0300

    relacionando api com o front

--- app/src/main/java/br/com/course/wheaterapp/MainActivity.kt
+++ app/src/main/java/br/com/course/wheaterapp/MainActivity.kt
@@ -1,15 +1,17 @@
 package br.com.course.wheaterapp
 
-import android.content.ContentValues.TAG
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import br.com.course.wheaterapp.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private val binding : ActivityMainBinding by lazy{
@@ -18,21 +20,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        fetchWeatherData()
+        fetchWeatherData("Sao paulo")
     }
 
-    private fun fetchWeatherData() {
+    private fun fetchWeatherData(cityName:String) {
         val retrofit  = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl("https://api.openweathermap.org/data/2.5/")
             .build().create(ApiInterface::class.java)
-        val response = retrofit.getWeatherData("Sao paulo", "701cacd84bf215e3802a69dc251adaf4", "metric")
+        val response = retrofit.getWeatherData(cityName, "701cacd84bf215e3802a69dc251adaf4", "metric")
         response.enqueue(object : Callback<WeatherApp>{
             override fun onResponse(call: Call<WeatherApp>, response: Response<WeatherApp>) {
                 val responseBody = response.body()
                 if(response.isSuccessful && responseBody != null){
                     val temperature = responseBody.main.temp.toString()
-                    Log.d("TAG", "onResponse: $temperature")
+                    val humidity = responseBody.main.humidity
+                    val windSpeed = responseBody.wind.speed
+                    val sunRise = responseBody.sys.sunrise
+                    val sunSet = responseBody.sys.sunset
+                    val seaLevel = responseBody.main.pressure
+                    val condition = responseBody.weather.firstOrNull()?.main?:"unknow"
+                    val maxTemp = responseBody.main.temp_max
+                    val minTemp = responseBody.main.temp_min
+
+                    binding.temp.text = "$temperature °C"
+                    binding.weather.text = condition
+                    binding.maxTemp.text = "Max temp: $maxTemp °C"
+                    binding.minTemp.text = "Min temp: $minTemp °C"
+                    binding.humidity.text = "$humidity %"
+                    binding.windSpeed.text = "$windSpeed m/s"
+                    binding.sunrise.text = "$sunRise"
+                    binding.sunset.text = "$sunSet"
+                    binding.sea.text = "$seaLevel hPa"
+                    binding.condition.text = condition
+                    binding.day.text = dayName(System.currentTimeMillis())
+                        binding.date.text = date()
+                        binding.cityName.text = "$cityName"
                 }
             }
 
@@ -41,5 +64,16 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+        }
+
+    private fun date(): String {
+        val sdf = SimpleDateFormat("dd MMMM YYYY", Locale.getDefault())
+        return sdf.format(Date())
+    }
+
+    fun dayName(timestamp:Long):String{
+        val sdf = SimpleDateFormat("EEEE", Locale.getDefault())
+        return sdf.format(Date())
     }
 }
